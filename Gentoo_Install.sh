c825fd4ce8c979f42530aacec231c2090bf99721
Bloodwolf1320 <39631273+Bloodwolf1320@users.noreply.github.com>
2021-05-22 10:16:51 +0000

    Add files via upload
    Progress updates

--- Gentoo_Install.sh
+++ Gentoo_Install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-#change to root
-sudo su
-
 #Create partitions
 #This script is intended to be run from a gentoo based gui live environment.
 #Partitions should be made with gparted before running this script. If installing
 #from command line please use cfdisk or whatever else you like BEFORE running the script
 
 #Forces the creation of the /mnt/gentoo directory which sometimes fails in gentoo based distros
-mkdir /mnt/gentoo
+#mkdir /mnt/gentoo
 
 #Formats the efi boot partition as fat32
 mkfs.vfat -F 32 /dev/sda1
@@ -22,7 +19,7 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt/gentoo
 
 #Sets the time
-ntpd -q -g
+#ntpd -q -g
 
 #Change directory into the gentoo directory on HDD
 cd /mnt/gentoo
@@ -35,3 +32,46 @@ tar xpvf stage3-amd64-20210519T214503Z.tar.xz --xattrs-include='*.*' --numeric-o
 
 #Opens nano text editor to configure the make.conf file.
 nano -w /mnt/gentoo/etc/portage/make.conf
+
+#Choose Mirror
+mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
+
+#Make repos.conf
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+
+#Copy latest Gentoo repository configuration to repo directory
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+
+#Copy DNS settings to retain Internet access
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+#Mount required filesystems
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+
+#chroot into system
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+
+#Mount Boot Partition
+mount /dev/sda1 /boot
+
+#Install latest ebuild snapshot
+emerge-webrsync
+
+#Update to latest ebuild repo after snapshot
+emerge --sync
+
+#List Profiles
+eselect profile list
+
+#To select a profile use eselect profile set followed by the profile number from the list.
+
+#Update world set
+emerge --ask --verbose --update --deep --newuse @world
+
+
